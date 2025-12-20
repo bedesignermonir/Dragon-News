58a27ad5d15b2d51482136e56913ae0f2a6dd901
bedesignermonir <clkmonir@gmail.com>
2025-12-20 16:28:48 +0600

    Date and Time added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import logo from "../assets/logo.png"
 
 
@@ -6,6 +7,7 @@ const Header = () => {
         <div className="flex items-center flex-col">
             <img src={logo} alt="" />
             <h3>Journalism Without Fear or Favour</h3>
+            <p className="font-bold">{format(new Date(), "EEEE, LLLL cc y")}</p>
         </div>
     );
 };
